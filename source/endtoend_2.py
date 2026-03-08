412b10501c8ddbe8a02941393ffa96ad5f5296a5
dsk <shaokangdu@buaa.edu.cn>
2026-03-08 23:23:56 +0800

    update

--- source/endtoend_2.py
+++ source/endtoend_2.py
@@ -3,36 +3,39 @@ import numpy as np
 
 # X-axis: sequence length
 x = [64, 128, 256, 512, 1024, 2048, 2560, 3072, 4096]
-
+fontsize = 12
 # Latency data
 matrixfold_fp16 = [16.45, 19.86, 36.3, 46.57, 130.13, 732, 1143, 1728, 3768]
 matrixfold_int8 = [16.95, 20, 37, 47, 132, 840, 1304, 2040, 4436]
+fastfold = [55.19, 60.62, 63.83, 83.66, 191.39, 946, 1618, 5007, 12455]
 open_omics = [24, 39.53, 88.18, 257.78, 682.7, 2367.43, 3654.44, 5445.42, 10560.44]
-fastfold = [128, 139.7, 143.8, 175, 329, 844, 1524, 4866, 12220]
+# fastfold = [128, 139.7, 143.8, 175, 329, 844, 1524, 4866, 12220]
 
 # Speedup over Open-Omics AlphaFold
 speedup_fp16 = [o / m for o, m in zip(open_omics, matrixfold_fp16)]
 speedup_int8 = [o / m for o, m in zip(open_omics, matrixfold_int8)]
 speedup_fastfold = [o / m for o, m in zip(open_omics, fastfold)]
+speedup_over_fastfold = [o / f for o, f in zip(fastfold, matrixfold_fp16)]
+print(max(speedup_over_fastfold), min(speedup_over_fastfold))
 print(max(speedup_fp16), min(speedup_fp16))
 
-fig, ax1 = plt.subplots()
+fig, ax1 = plt.subplots(figsize=(8, 4))
 
 # --------------------
 # Left Y-axis: Latency
 # --------------------
-ax1.plot(x, matrixfold_fp16, marker='o', label='MatrixFold (FP16) Latency')
-ax1.plot(x, matrixfold_int8, marker='o', label='MatrixFold (INT8) Latency')
-ax1.plot(x, fastfold, marker='o', label='FastFold Latency')
-ax1.plot(x, open_omics, marker='o', 
-         label='Open-Omics AlphaFold Latency')
+ax1.semilogy(x, matrixfold_fp16, marker='o', label='MatrixFold Latency', color='#d62728')
+# ax1.semilogy(x, matrixfold_int8, marker='o', label='MatrixFold (INT8) Latency')
+ax1.semilogy(x, fastfold, marker='o', label='FastFold Latency')
+ax1.semilogy(x, open_omics, marker='o', 
+         label='Open-Omics AlphaFold Latency', color='green')
 
-ax1.set_xlabel('Sequence Length')
-ax1.set_ylabel('Latency (s)')
+ax1.set_xlabel('Sequence Length', fontsize=fontsize)
+ax1.set_ylabel('Latency (s)', fontsize=fontsize)
 # lat_max = max(fastfold)
 
-ax1.set_ylim(-1000, 13000)
-ax1.set_yticks([0, 2000, 4000, 6000, 8000, 10000, 12000])
+# ax1.set_ylim(-1000, 13000)
+# ax1.set_yticks([0, 2000, 4000, 6000, 8000, 10000, 12000])
 # ax1.set_ylim(0, lat_max)
 # num_ticks = 6
 # lat_ticks = np.linspace(0, lat_max, num_ticks)
@@ -45,14 +48,14 @@ ax1.grid(True)
 # --------------------
 ax2 = ax1.twinx()
 ax2.plot(x, speedup_fp16, marker='x', linestyle=':',
-         label='MatrixFold (FP16) Speedup')
-ax2.plot(x, speedup_int8, marker='x', linestyle=':',
-         label='MatrixFold (INT8) Speedup')
+         label='MatrixFold Speedup', color='#d62728')
+# ax2.plot(x, speedup_int8, marker='x', linestyle=':',
+        #  label='MatrixFold (INT8) Speedup')
 ax2.plot(x, speedup_fastfold, marker='x', linestyle=':',
          label='FastFold Speedup')
 
 ax2.set_ylabel('Speedup over Open-Omics AlphaFold')
-ax2.axhline(1.0, linestyle='--', color='#d62728')
+ax2.axhline(1.0, linestyle='--', color='green')
 
 spd_max = max(max(speedup_fp16), max(speedup_int8), max(speedup_fastfold))
 ax2.set_ylim(0, spd_max)
@@ -69,7 +72,7 @@ ax2.set_yticks([0, 1, 2, 3, 4, 5, 6])
 # --------------------
 lines1, labels1 = ax1.get_legend_handles_labels()
 lines2, labels2 = ax2.get_legend_handles_labels()
-ax1.legend(lines1 + lines2, labels1 + labels2, loc='upper right', framealpha=0.5)
+ax1.legend(lines1 + lines2, labels1 + labels2, loc='upper right', framealpha=0.5, fontsize=fontsize)
 
 plt.tight_layout()
 plt.savefig('../figures/overall_performance.pdf')
--- source/scale.py
+++ source/scale.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-fontsize=16
+fontsize=17
 
 # 数据
 numa = [1, 2, 4, 8]
@@ -22,18 +22,18 @@ fastfold = {512: 175, 1024: 329, 1576: 500}
 open_omics = {512: 257.78, 1024: 682.7, 1576: 1559.32}
 
 # 创建子图，每个子图纵轴独立
-fig, axes = plt.subplots(1, 3, figsize=(18,5.5), sharey=False)
+fig, axes = plt.subplots(1, 3, figsize=(18,4), sharey=False)
 
 for i, seq in enumerate(seq_lengths):
     ax = axes[i]
     
     # 绘制 FP16 和 INT8
-    ax.plot(numa, fp16[seq], marker='o', label='FP16')
-    ax.plot(numa, int8[seq], marker='s', label='INT8')
+    ax.plot(numa, fp16[seq], marker='o', label='MatrixFold', color='#d62728')
+    # ax.plot(numa, int8[seq], marker='s', label='INT8')
     
     # 绘制 fastfold 和 open-omics-alphafold 虚横线
-    ax.axhline(fastfold[seq], color='#2ca02c', linestyle='--', label='FastFold' if i==0 else "")
-    ax.axhline(open_omics[seq], color='#d62728', linestyle='--', label='Open-Omics-Alphafold' if i==0 else "")
+    ax.axhline(fastfold[seq], linestyle='--', label='FastFold' if i==2 else "")
+    ax.axhline(open_omics[seq], color='green', linestyle='--', label='Open-Omics-Alphafold' if i==2 else "")
     
     ax.set_title(f'Sequence length {seq}', fontsize=fontsize)
     ax.set_xlabel('NUMA nodes', fontsize=fontsize)
@@ -42,8 +42,8 @@ for i, seq in enumerate(seq_lengths):
     ax.set_yticklabels([int(x) for x in ax.get_yticks()], fontsize=fontsize)
     ax.grid(True, linestyle=':', alpha=0.5)
 
-axes[0].set_ylabel('Time (s)', fontsize=fontsize)
-axes[0].legend(loc='upper left')
+axes[2].set_ylabel('Time (s)', fontsize=fontsize)
+axes[2].legend(loc='upper right',fontsize=fontsize)
 # plt.suptitle('Performance across NUMA nodes and sequence lengths')
 plt.tight_layout()
 plt.savefig('../figures/scale.pdf')
